4c7663928c2b25c1b0efdacc2f4bdd895b790c3c
Mike Hewitt <matroxmike@icloud.com>
2018-10-25 14:22:40 +1100

    learned simple effect
    nice re-start to  several clojure projects

--- src/javaImport1/core.clj
+++ src/javaImport1/core.clj
@@ -10,6 +10,7 @@
    (println n)
    (println (Date. (long (.nextInt (Random.)))))
    (println (Date. n))
+   (println (new Date))
 )
 (comment   produces #inst "1970-01-04T14:55:16.377-00:00"   )
-(comment need to change to produce today's date)
+(comment need to change to produce today's date  );; so    (println (new Date)))
